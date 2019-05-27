3b9638d9be366a61227cc0a67ac7a4686292c0c7
苏锡波 <007@dotnetws.com>
2019-05-27 18:58:22 +0800

    Update.

---
+++ src/ATBase.Core/SuperMap.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ATBase.Core
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public static class SuperMap
+    {
+        private static ConcurrentDictionary<Type, Object> _cache = new ConcurrentDictionary<Type, Object>();
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="keyValues">键值对</param>
+        public static T Map<T>(IDictionary<String, Object> keyValues) where T : new()
+        {
+            var func = GetSuperFunc<T>();
+            if (func != null)
+            {
+                return func(keyValues);
+            }
+
+            return default(T);
+        }
+
+        private static Func<IDictionary<String, Object>, T> GetSuperFunc<T>()
+        {
+            if (!_cache.TryGetValue(typeof(T), out Object value))
+            {
+                StringBuilder sb = new StringBuilder();
+                sb.AppendLine("");
+            }
+
+            return value as Func<IDictionary<String, Object>, T>;
+        }
+    }
+}
